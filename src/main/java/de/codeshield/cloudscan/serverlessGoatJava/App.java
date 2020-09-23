85e7271b85f3c89bdc20a30c4fbecbdd1e26bc23
Manuel Benz <manuel.benz@codeshield.de>
2020-09-23 12:03:04 +0200

    finish up java implementation of function

--- src/main/java/de/codeshield/cloudscan/serverlessGoatJava/App.java
+++ src/main/java/de/codeshield/cloudscan/serverlessGoatJava/App.java
@@ -6,26 +6,32 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
 import de.codeshield.cloudscan.serverlessGoatJava.apiGateway.ApiGatewayRequest;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
-import software.amazon.awssdk.services.s3.S3Client;
+import java.util.UUID;
+import javax.ws.rs.core.Response;
 
 /**
- * Lambda function entry point. You can change to use other pojo type or implement
- * a different RequestHandler.
+ * Lambda function entry point. You can change to use other pojo type or implement a different
+ * RequestHandler.
  *
- * @see <a href=https://docs.aws.amazon.com/lambda/latest/dg/java-handler.html>Lambda Java Handler</a> for more information
+ * @see <a href=https://docs.aws.amazon.com/lambda/latest/dg/java-handler.html>Lambda Java
+ *     Handler</a> for more information
  */
-public class App implements RequestHandler<ApiGatewayRequest, Object> {
-  private final S3Client s3Client;
+public class App implements RequestHandler<ApiGatewayRequest, Response> {
+  private final AmazonS3 s3Client;
   private final Path catdocExecutable;
 
   public App() throws IOException {
-    // Initialize the SDK client outside of the handler method so that it can be reused for subsequent invocations.
+    // Initialize the SDK client outside of the handler method so that it can be reused for
+    // subsequent invocations.
     // It is initialized when the class is loaded.
     s3Client = DependencyFactory.s3Client();
 
@@ -36,9 +42,8 @@ public class App implements RequestHandler<ApiGatewayRequest, Object> {
 
   private void log(Context context, ApiGatewayRequest request) {
 
-    AmazonDynamoDB docClient = AmazonDynamoDBClientBuilder.standard()
-            .withRegion(Regions.EU_CENTRAL_1)
-            .build();
+    AmazonDynamoDB docClient =
+        AmazonDynamoDBClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build();
 
     String requestId = context.getAwsRequestId();
     String ip = request.getRequestContext().getIdentity().getSourceIp();
@@ -53,26 +58,38 @@ public class App implements RequestHandler<ApiGatewayRequest, Object> {
   }
 
   @Override
-  public Object handleRequest(ApiGatewayRequest request, final Context context) {
-    log(context, request);
-
-    String documentUrl = request.getQueryStringParameters().get("document_url");
-    String txt = getDocumentText(documentUrl);
-
-    // TODO: invoking the api call using s3Client.
-    return request;
-  }
-
-  private String getDocumentText(String documentUrl) {
-
-    String command = String.format("curl --silent -L %s | /lib64/ld-linux-x86-64.so.2 %s -", documentUrl, catdocExecutable.toAbsolutePath().toString());
-
+  public Response handleRequest(ApiGatewayRequest request, final Context context) {
     try {
-      Process process = new ProcessBuilder("/bin/sh", "-c", command).start();
-      return com.amazonaws.util.IOUtils.toString(process.getInputStream());
-    } catch (IOException e) {
-      throw new RuntimeException("Could not execute catdoc");
+      log(context, request);
+
+      String documentUrl = request.getQueryStringParameters().get("document_url");
+      String txt = getDocumentText(documentUrl);
+
+      // Lambda response max size is 6MB. The workaround is to upload result to S3 and redirect user
+      // to the file.
+      String key = UUID.randomUUID().toString();
+      // FIXME might not be public-read
+      s3Client.putObject(System.getenv("BUCKET_NAME"), key, txt);
+
+      String docLocation = String.format("%s/%s", System.getenv("BUCKET_URL"), key);
+      return Response.status(302).header("Location", docLocation).build();
+
+    } catch (Throwable e) {
+      StringWriter sw = new StringWriter();
+      e.printStackTrace(new PrintWriter(sw));
+      String exceptionAsString = sw.toString();
+      return Response.status(500).entity(exceptionAsString).build();
     }
   }
 
+  private String getDocumentText(String documentUrl) throws IOException {
+
+    String command =
+        String.format(
+            "curl --silent -L %s | /lib64/ld-linux-x86-64.so.2 %s -",
+            documentUrl, catdocExecutable.toAbsolutePath().toString());
+
+    Process process = new ProcessBuilder("/bin/sh", "-c", command).start();
+    return com.amazonaws.util.IOUtils.toString(process.getInputStream());
+  }
 }
--- src/main/java/de/codeshield/cloudscan/serverlessGoatJava/DependencyFactory.java
+++ src/main/java/de/codeshield/cloudscan/serverlessGoatJava/DependencyFactory.java
@@ -1,27 +1,16 @@
 package de.codeshield.cloudscan.serverlessGoatJava;
 
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
-import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
-
-/**
- * The module containing all dependencies required by the {@link App}.
- */
+/** The module containing all dependencies required by the {@link App}. */
 public class DependencyFactory {
 
-  private DependencyFactory() {
-  }
+  private DependencyFactory() {}
 
-  /**
-   * @return an instance of S3Client
-   */
-  public static S3Client s3Client() {
-    return S3Client.builder()
-            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
-            .region(Region.EU_CENTRAL_1)
-            .httpClientBuilder(UrlConnectionHttpClient.builder())
-            .build();
+  /** @return an instance of S3Client */
+  public static AmazonS3 s3Client() {
+    return AmazonS3ClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build();
   }
 }
